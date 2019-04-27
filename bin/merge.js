b9737e4179c2f1623a44085ea1f98e11e9faaab6
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-04-27 23:31:32 +0200

    Upgrade ava and xo + coding style

--- bin/merge.js
+++ bin/merge.js
@@ -11,13 +11,16 @@ function getDepartements() {
   if (!process.env.DEPARTEMENTS) {
     return codesDepartements
   }
+
   const departements = process.env.DEPARTEMENTS.split(',')
   if (departements.length === 0) {
     throw new Error('La liste de départements fournie est mal formée')
   }
+
   if (departements.some(codeDep => !codesDepartements.includes(codeDep))) {
     throw new Error('La liste de départements fournie est invalide')
   }
+
   return departements
 }
 
