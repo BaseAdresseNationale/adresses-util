fa1391f09736f64d2463fb686f743154bd95da28
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-04-30 13:00:35 +0200

    Use @etalab/fantoir

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const voies = require('./lib/voies')
 const {extractNumeroSuffixe, rewriteSuffixes} = require('./lib/numeros')
-const {createMatcher} = require('./lib/fantoir-match')
 const bal = require('./lib/bal')
 
 module.exports = {
   ...voies,
   ...bal,
   rewriteSuffixes,
-  createFantoirMatcher: createMatcher,
   extractNumeroSuffixe
 }
