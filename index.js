5af1e41ffc0f00065558ebfb39b724695bd600ea
gllmr <guillaume@livingdata.co>
2022-09-02 16:41:03 +0200

    Add getPositionPriorityByType function

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const voies = require('./lib/voies')
-const {extractNumeroSuffixe, rewriteSuffixes} = require('./lib/numeros')
+const {extractNumeroSuffixe, rewriteSuffixes, getPositionPriorityByType} = require('./lib/numeros')
 const bal = require('./lib/bal')
 
 module.exports = {
   ...voies,
   ...bal,
   rewriteSuffixes,
-  extractNumeroSuffixe
+  extractNumeroSuffixe,
+  getPositionPriorityByType
 }
